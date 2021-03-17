14a86573944718a69c960bbf6d4e07e9c1c4b824
SeeratArora <50323628+SeeratArora@users.noreply.github.com>
2021-03-17 16:53:04 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -37,3 +37,9 @@ $pathArgs = {C:\dotnet-sdk-3.1.406-win-x64.exe /Install /Quiet /Norestart /Logs 
 Invoke-Command -ScriptBlock $pathArgs
 
 Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
+
+
+# Download and install Data Mirgation Assistant
+(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
+Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
+
